ae462181b9f967b739436943501eeabb2d969df5
Yaps960 <155361632+Yaps960@users.noreply.github.com>
2024-02-02 06:37:01 +0800

    test.cpp

--- test.cpp
+++ test.cpp
@@ -1,76 +1,170 @@
-//kulang: error handling, do while
-//mewing bye bye, array error, CHECKED
-//parameter, ipapasa pointer ng array
-
 #include <iostream>
 
 using namespace std;
 
-int WaitingTime();
-int TurnAroundTime();
-int Menu();
-void details(int choice, int numProcess);
-void banner();
-void SRTF();
-void PS();
-void RR();
+void print(int numProcess, int arrTime[], int burTime[], int Priority[], int quantumTime, int CTime[], int TurnTime[], int WaitTime[], int choice);
+void details(int choice, int numProcess, int arrTime[], int burTime[], int Priority[], int *quantumTime);
+void calculateTurnAndWait(int arrTime[], int burTime[], int CTime[], int WaitTime[], int TurnTime[], int numProcess);
+int HighestBurtime(int *burTime, int numProcess);
 
-int main(){
-    int numProcess;
-	int choice = Menu();
-    if(choice == 1)
-		cout << "Shortest Remaining Time First\n";
-    else if(choice == 2)
-		cout << "Priority Scheduling\n";
-    else if(choice == 3)
-		cout << "Round Robin\n";
-    if(choice == 1 || (choice == 2 || choice == 3)){
-        cout << "Number of Processes: ";
-        cin >> numProcess;
-        int mew[numProcess];
-        int arrTime[numProcess];
-        int burTime[numProcess];
-        int Priority[numProcess];
-        details(choice, numProcess);
-        for(int i = 0; i < sizeof(mew) / sizeof(mew[0]); i++){
-            cout << i << '\n';
+int main()
+{
+    int exitFlag = 0;
+    int choice;
+    do
+    {
+        cout << "1. Shortest Remaining Time First\n2. Priority Scheduling\n3. Round Robin\n4. Exit\nEnter your option: ";
+        cin >> choice;
+        if (choice < 1 || choice > 4 || !choice)
+        {
+            cout << "Invalid Input\n";
+            cin.clear();
+            cin.ignore();
         }
+        cout << '\n';
+        int numProcess;
+        if(choice >= 1 && choice <=3){
+            do{
+            cout << "Number of Processes: ";
+            cin >> numProcess;
+            }while(!numProcess || numProcess <= 0);
+        }
+            int arrTime[numProcess], burTime[numProcess], Priority[numProcess], CTime[numProcess], TurnTime[numProcess], WaitTime[numProcess];
+            int quantumTime, lowest, done;
+            int tick = 0, mew = 0;
+            int copyBurTime[numProcess];
+        if(choice >= 1 && choice <=3){
+            details(choice, numProcess, arrTime, burTime, Priority, &quantumTime);
+            for(int i = 0; i < numProcess; i++){
+                copyBurTime[i] = burTime[i];
+            }
+        }
+        copyBurTime[numProcess] = 9999;
+        switch (choice)
+        {
+        case 1:
+            do
+            {
+                lowest = numProcess;
+                for(int i=0; i<numProcess; i++)
+                {
+                    if(arrTime[i]<=tick && copyBurTime[i]< copyBurTime[lowest] && copyBurTime[i]>0 )
+                        lowest=i;
+                }
+                copyBurTime[lowest]--;
+                
+                if(copyBurTime[lowest] == 0){
+                    mew++;
+                    done = tick + 1;
+                    CTime[lowest] = done;
+                }
+                tick++;
+            }while(mew!=numProcess);
+            calculateTurnAndWait(arrTime, burTime, CTime, WaitTime, TurnTime, numProcess);
+            print(numProcess, arrTime, burTime, Priority, quantumTime, CTime, TurnTime, WaitTime, choice);
+            break;
+        case 2:
+            do{
+                lowest = numProcess;
+                for(int i=0; i<numProcess; i++)
+                {
+                    if(arrTime[i] <= tick && Priority[i]<Priority[lowest] && copyBurTime[i]>0 )
+                        lowest=i;
+                }
+                copyBurTime[lowest]--;
+
+                if(copyBurTime[lowest]==0)
+                {
+                    mew++;
+                    done=tick + 1;
+                    CTime[lowest] = done;
+                }
+                tick++;
+            }while(mew != numProcess);
+            calculateTurnAndWait(arrTime, burTime, CTime, WaitTime, TurnTime, numProcess);
+            print(numProcess, arrTime, burTime, Priority, quantumTime, CTime, TurnTime, WaitTime, choice);
+            break;
+        case 3:
+            break;
+        case 4:
+            exitFlag = 1;
+            break;
+        }
+    } while (exitFlag != 1);
+
+    return 0;
+}
+
+
+
+
+void print(int numProcess, int arrTime[], int burTime[], int Priority[], int quantumTime, int CTime[], int TurnTime[], int WaitTime[], int choice){
+    double aveWait, aveTurn;
+    cout << "Process" << "\t" << "Arrival Time" << "\t" << "Burst Time" << "\t";
+    if(choice == 2)
+        cout << "Priority Level";
+    cout << "\t" << "Waiting Time" << "\t" << "Turnaround Time" << "\t"<<"Completion Time" << endl;
+    if(choice == 3)
+        cout << "Quantum Time = " << quantumTime;
+    for(int i=0; i<numProcess; i++)
+    {
+        cout << "p" << i << "\t\t" << arrTime[i] << "\t\t" << burTime[i] << "\t\t";
+        if(choice == 2)
+            cout << Priority[i] << "\t\t";
+        cout << WaitTime[i] << "\t\t" << TurnTime[i] << "\t\t" << CTime[i] << endl;
+        aveWait = aveWait + WaitTime[i];
+        aveTurn = aveTurn + TurnTime[i];
     }
-	return 0;
+    cout << "\nAverage Turnaround Time = " << aveTurn/numProcess << "m/s";
+    cout << "\nAverage Waiting Time = " << aveWait/numProcess << "m/s\n\n";
 }
 
-int Menu(){
-	banner();
-	int choice;
-	cout << "1. Shortest Remaining Time First\n2. Priority Scheduling\n3. Round Robin\nEnter your option: ";
-	cin >> choice;
-	cout << '\n';
-	return choice;
+
+
+
+void details(int choice, int numProcess, int arrTime[], int burTime[], int Priority[], int *quantumTime){
+    for (int i = 0; i < numProcess; i++)
+    {
+        cout << "Process #" << i + 1 << '\n';
+        cout << "Arrival Time: ";
+        cin >> arrTime[i];
+        cout << "Burst Time: ";
+        cin >> burTime[i];
+        if (choice == 2)
+        {
+            cout << "Priority: ";
+            cin >> Priority[i];
+        }
+        cout << '\n';
+    }
+    if (choice == 3)
+    {
+        int highestBurTime = burTime[0];
+        for (int i = 1; i < numProcess; i++)
+        {
+            if (highestBurTime < burTime[i])
+                highestBurTime = burTime[i];
+        }
+        do
+        {
+            cout << "Quantum Time: ";
+            cin >> *quantumTime;
+            if (*quantumTime > highestBurTime)
+            {
+                cout << "Invalid Input\n";
+                cin.clear();
+                cin.ignore();
+            }
+        } while (!*quantumTime || *quantumTime > highestBurTime);
+    }
 }
 
-void banner(){	
-	cout << ".d8888.  .o88b. db   db d88888b d8888b. db    db db      d888888b d8b   db  d888b  \n";
-	cout << "88'  YP d8P  Y8 88   88 88'     88  `8D 88    88 88        `88'   888o  88 88' Y8b \n";
-	cout << "`8bo.   8P      88ooo88 88ooooo 88   88 88    88 88         88    88V8o 88 88      \n";
-	cout << "  `Y8b. 8b      88~~~88 88~~~~~ 88   88 88    88 88         88    88 V8o88 88  ooo \n";
-	cout << "db   8D Y8b  d8 88   88 88.     88  .8D 88b  d88 88booo.   .88.   88  V888 88. ~8~ \n";
-	cout << "`8888Y'  `Y88P' YP   YP Y88888P Y8888D' ~Y8888P' Y88888P Y888888P VP   V8P  Y888P  \n\n";
-}
 
-void details(int choice, int numProcess){
-	for(int i = 0; i < numProcess; i++){
-		cout << "Process #" << i+1 << '\n';
-		cout << "Arrival Time: ";
-		cin >> arrTime;
-		cout << "Burst Time: ";
-		cin >> burTime
-		if(choice == 2){
-			cout << "Priority: ";
-			cin >> Priority;
-		}
-		cout << '\n';
-	}
-}
 
-void SRTF(){
+
+void calculateTurnAndWait(int arrTime[], int burTime[], int CTime[], int WaitTime[], int TurnTime[], int numProcess){
+    for(int i = 0; i < numProcess; i++)
+        TurnTime[i] = CTime[i] - arrTime[i];
+    for(int i = 0; i < numProcess; i++)
+        WaitTime[i] = TurnTime[i] - burTime[i];
 }
